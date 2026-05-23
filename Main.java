1230cd329f2b3bd9b33bc847bd281ae4b05773bf
mansoorjr <mr.mirzad4@gmail.com>
2026-05-22 18:02:21 -0700

    Update Main.java for week 5

--- Main.java
+++ Main.java
@@ -3,51 +3,62 @@
  * 
  * @author Mohammad Mansoor Mirzad
  * 
- * Date:4/30 2026
+ * Date:5/22 2026
  * 
  * This class is just for testing.
  */
 public class Main {
 
-    public static void main(String[] args) {
+	// main method
+	public static void main(String[] args) {
 
-        try {
+	    try {
 
-            GradeTracker tracker = new GradeTracker();
+	        GradeTracker tracker =
+	                new GradeTracker();
 
-            Student student = new Student("Mohammad");
+	        Student student =
+	                new Student("Mohammad");
 
-            Course course = new Course("Java");
+	        Course course =
+	                new Course("Java");
 
-            // add valid grades
-            course.addGrade(new Grade(90));
-            course.addGrade(new Grade(85));
+	        // add grades
+	        course.addGrade(new Grade(90));
 
-            // HAS-A: Student has a Course
-            student.addCourse(course);
+	        course.addGrade(new Grade(85));
 
-            // HAS-A: GradeTracker has a Student
-            tracker.addStudent(student);
+	        // HAS-A: Student has a Course
+	        student.addCourse(course);
 
-            Student foundStudent =
-                    tracker.findStudent("Mohammad");
+	        // HAS-A: GradeTracker has a Student
+	        tracker.addStudent(student);
 
-            if (foundStudent != null) {
+	        // search student
+	        Student foundStudent =
+	                tracker.findStudent("Mohammad");
 
-                Course foundCourse =
-                        foundStudent.findCourse("Java");
+	        // check if found
+	        if (foundStudent != null) {
 
-                if (foundCourse != null) {
+	            Course foundCourse =
+	                    foundStudent.findCourse("Java");
 
-                    System.out.println(
-                            "Average Grade: "
-                                    + foundCourse.getAverage());
-                }
-            }
+	            // check if found
+	            if (foundCourse != null) {
 
-        } catch (InvalidGradeException e) {
+	                // show average
+	                System.out.println(
+	                        "Average Grade: "
+	                                + foundCourse.getAverage());
+	            }
+	        }
 
-            System.out.println(e.getMessage());
-        }
-    }
+	    } catch (InvalidGradeException e) {
+
+	        // show error
+	        System.out.println(
+	                e.getMessage());
+	    }
+	}
 }
