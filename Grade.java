6094b78a46ed53deee732403614d44369b9a45cd
mansoorjr <mr.mirzad4@gmail.com>
2026-05-22 17:58:00 -0700

    Update Grade.java for week 5

--- Grade.java
+++ Grade.java
@@ -3,27 +3,32 @@
  * 
  * @author Mohammad Mansoor Mirzad
  * 
- * Date:4/30 2026
+ * Date:5/22 2026
  * 
  * This class stores one grade.
  */
 public class Grade {
 
-    private double value; // stores grade value
+	private double value; // stores grade value
 
-    // constructor checks if grade is valid
-    public Grade(double value) throws InvalidGradeException {
+	// constructor checks grade
+	public Grade(double value)
+	        throws InvalidGradeException {
 
-        // throw exception if grade is invalid
-        if (value < 0 || value > 100) {
-            throw new InvalidGradeException(
-                    "Grade must be between 0 and 100.");
-        }
+	    // check if invalid
+	    if (value < 0 || value > 100) {
 
-        this.value = value; // save valid grade
-    }
+	        // throw custom exception
+	        throw new InvalidGradeException(
+	                "Grade must be between 0 and 100.");
+	    }
 
-    public double getValue() {
-        return value; // return grade value
-    }
+	    this.value = value; // save valid grade
+	}
+
+	// return grade value
+	public double getValue() {
+
+	    return value;
+	}
 }
