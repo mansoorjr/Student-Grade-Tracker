d6cde4dc1a0a7dbb49ae1f48da6478cc2eccf2da
mansoorjr <mr.mirzad4@gmail.com>
2026-05-31 12:19:39 -0700

    week 8 update

--- FileManager.java
+++ FileManager.java
@@ -1,41 +1,77 @@
-import java.io.FileWriter; // used to write to a file
+
+import java.io.FileWriter; // used to write to file
 import java.io.IOException; // used for file errors
+import java.io.BufferedReader; // used to read file
+import java.io.FileReader; // used to open file
 
 /**
  * Lead Author(s):Mohammad Mansoor Mirzad
  * 
- * @author Allan Schougaard 
+ * Professor:Allan Schougaard
  * 
- * Other contributors:None                                    
+ * References:
+ *         Oracle Java Documentation
+ *         https://docs.oracle.com/javase/tutorial/essential/io/
  * 
- * References:Oracle Java Documentation (Swing GUI):
- *         https://docs.oracle.com/javase/tutorial/uiswing/
+ * Version/date:5/31/2026
  * 
- * Coding with John (YouTube):
- *         https://www.youtube.com/watch?v=aqcJsKdjjvU
- *         
- * Version/date: 5/28 2026
- * 
- * Responsibilities of class:This class saves data to a text file.
+ * Responsibilities of class:
+ *         This class saves and loads
+ *         data from a text file.
  */
+
+// HAS-A: FileManager uses a FileWriter object.
+// HAS-A: FileManager uses a FileReader object.
 public class FileManager {
 
-	// save data into file
-	public void saveData(String text) {
+    // save data into file
+    public void saveData(String text) {
 
-	    // FileWriter closes automatically
-	    try (FileWriter writer =
-	                 new FileWriter("grades.txt")) {
+        // FileWriter closes automatically
+        try (FileWriter writer =
+                     new FileWriter("grades.txt")) {
 
-	        writer.write(text); // write text
+            // write text into file
+            writer.write(text);
 
-	        System.out.println("Data saved.");
+            // show success message
+            System.out.println(
+                    "Data saved.");
 
-	    } catch (IOException e) {
+        } catch (IOException e) {
 
-	        // show file error
-	        System.out.println(
-	                "Error saving file.");
-	    }
-	}
+            // show file error
+            System.out.println(
+                    "Error saving file.");
+        }
+    }
+
+    // load data from file
+    public String loadData() {
+
+        StringBuilder data =
+                new StringBuilder();
+
+        try (BufferedReader reader =
+                     new BufferedReader(
+                             new FileReader(
+                                     "grades.txt"))) {
+
+            String line;
+
+            // read file line by line
+            while ((line = reader.readLine())
+                    != null) {
+
+                data.append(line)
+                        .append("\n");
+            }
+
+        } catch (IOException e) {
+
+            return "Error loading file.";
+        }
+
+        return data.toString();
+    }
 }
